5fa1a0029a980d558c4f95a6325ff1d31ce3d650
Thanh Nguyen <chrisng9@outlook.com>
2022-06-03 12:14:37 -0700

    checking in sec9 p72

---
+++ sec9/p72/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"crypto/sha256"
+	"hash"
+	"html/template"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+type user struct {
+	UserName string
+	Password hash.Hash
+	First    string
+	Last     string
+}
+
+var dbUsers = map[string]user{}      // user ID, user
+var dbSessions = map[string]string{} // session ID, user ID
+
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/sigup", signup)
+	http.HandleFunc("/login", login)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	u := getUser(w, r)
+	tmpl.ExecuteTemplate(w, "index.html", u)
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	u := getUser(w, r)
+	if !alreadyLoggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	tmpl.ExecuteTemplate(w, "about.html", u)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	if alreadyLoggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	var u user
+	// process from submission
+	if r.Method == http.MethodPost {
+		// get form values
+		un := r.FormValue("username")
+		p := r.FormValue("password")
+		f := r.FormValue("firtname")
+		l := r.FormValue("lastname")
+
+		// username taken?
+		if _, ok := dbUsers[un]; ok {
+			http.Error(w, "Username already taken", http.StatusForbidden)
+			return
+		}
+		// create session
+		uuid := uuid.New()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: uuid.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+
+		// store user in dbUsers
+		bs := sha256.New()
+		bs.Write([]byte(p))
+		u = user{
+			un, bs, f, l,
+		}
+		dbUsers[un] = u
+
+		// redirect
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	tmpl.ExecuteTemplate(w, "signup.html", u)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if alreadyLoggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	// process form submission
+	if r.Method == http.MethodPost {
+		un := r.FormValue("username")
+		p := r.FormValue("password")
+		// is there a username?
+		u, ok := dbUsers[un]
+		if !ok {
+			http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+		// does the entered password match the stored password?
+		if !u.Password {
+			http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+		// create session
+		uuid := uuid.New()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: uuid.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	tmpl.ExecuteTemplate(w, "login.html", nil)
+}
