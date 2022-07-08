21b185532280a64558c172b83c56765e18b21a62
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-07-08 04:47:33 -0700

    adding exercises code

--- exercises/httpserverex1/main.go
+++ exercises/httpserverex1/main.go
@@ -1,4 +1,4 @@
-// TCP server - http request/response
+// exercise - create the http server that will response the URL of the request
 package main
 
 import (
@@ -8,6 +8,60 @@ import (
 	"net"
 	"strings"
 )
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	// read request
+	request(conn)
+
+	// write respond
+	respond(conn)
+}
+
+// request receives the http request, scan the content of the request
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			// request line
+			m := strings.Fields(ln)[0]
+			u := strings.Fields(ln)[1]
+			fmt.Println("Requested URL:", u)
+			fmt.Println("***METHOD***", m)
+		}
+		if ln == "" {
+			// header are done
+			break
+		}
+		i++
+	}
+}
+
+// respond returns the http header information and the contest from the
+// request URL
+func respond(conn net.Conn) {
+	body := `
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<h1>Exercise - http server that respond the requested URL</h1>
+</body>
+</html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
 
 func main() {
 	listen, err := net.Listen("tcp", ":8080")
@@ -23,53 +77,4 @@ func main() {
 		}
 		go handle(conn)
 	}
-}
-
-func handle(conn net.Conn) {
-	defer conn.Close()
-
-	// read request
-	request(conn)
-
-	// write respond
-	respond(conn)
-}
-
-func request(conn net.Conn) {
-	i := 0
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		ln := scanner.Text()
-		fmt.Println(ln)
-		if i == 0 {
-			// request line
-			m := strings.Fields(ln)[0]
-			fmt.Println("***METHOD***", m)
-		}
-		if ln == "" {
-			// header are done
-			break
-		}
-		i++
-	}
-}
-
-func respond(conn net.Conn) {
-	body := `
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title></title>
-</head>
-<body>
-	<strong>Hello World!</strong>
-</body>
-</html>`
-
-	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
-	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
-	fmt.Fprint(conn, "Content-Type: text/html\r\n")
-	fmt.Fprint(conn, "\r\n")
-	fmt.Fprint(conn, body)
 }
---
+++ exercises/httpserverex1refact/main.go
@@ -0,0 +1,145 @@
+// exercise - write you own http multiplexer for routing http requests
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	// read request
+	request(conn)
+
+	// write respond
+	// respond(conn)
+}
+
+// request receives the http request, scan the content of the request
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			mux(conn, ln)
+		}
+		if ln == "" {
+			// header are done
+			break
+		}
+		i++
+	}
+}
+
+// respond returns the http header information and the contest from the
+// request URL
+func home(conn net.Conn) {
+	body := `
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+</head>
+<body>
+	<h1>Exercise - http server that respond the requested URL</h1>
+	<h2>This is the home page</h2>
+</body>
+</html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func about(conn net.Conn) {
+	body := `
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>About</title>
+</head>
+<body>
+	<h1>Exercise - http server that respond the requested URL</h1>
+	<h2>This is the about page</h2>
+</body>
+</html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func badUrl(conn net.Conn) {
+	body := `
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Bad URL</title>
+</head>
+<body>
+	<h1>Exercise - http server that respond the requested URL</h1>
+	<h2>Oops, I don't have what you are looking for!</h2>
+</body>
+</html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 400 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func mux(conn net.Conn, ln string) {
+	connect := conn
+
+	// request line
+	m := strings.Fields(ln)[0] // method
+	u := strings.Fields(ln)[1] // uri
+
+	fmt.Println("***METHOD", m)
+	fmt.Println("***URI", u)
+
+	// Mux logic
+	if ln == "/" || ln == "/home" {
+		fmt.Println("Requested URL:", u)
+		fmt.Println("***METHOD***", m)
+		home(connect)
+	} else if u == "/about" {
+		fmt.Println("Requested URL:", u)
+		fmt.Println("***METHOD***", m)
+		about(connect)
+	} else {
+		fmt.Println("Requested URL:", u)
+		fmt.Println("***METHOD***", m)
+		badUrl(connect)
+	}
+}
+
+func main() {
+	listen, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer listen.Close()
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		go handle(conn)
+	}
+}
